0f21aef38b38056dbf744839db007c2e86be1809
KseniaShkaruta <46504542+KseniaShkaruta@users.noreply.github.com>
2019-01-21 19:45:04 -0500

    Update DELETION.py
    Completed code for DELETION 
    Changes
    1. Used apply/lambda to iterate through df to manipulate 'json_utterances-man' and output a new transcript without the deleted words (json format that contains only word tokens but not 'starting time')
    2. Output updated df into a new csv file
    3. Created a main function that includes all other functions to do the deletion manipulations
    4. Altered code for other functions to accommodate df iteration
    
    Outstanding:
    1. Does not output list of the deleted words into df
    2. Current code does not handle Null values for 'json_utterances-man'

--- DELETION.py
+++ DELETION.py
@@ -2,22 +2,33 @@ import pandas as pd
 import json
 import random
 
+#read in csv file, delete 20% of words from json manual transcript, output csv with altered manual transcript as new column
+df = pd.read_csv('C:/temp/ASRforAD.csv')
+#df['json_with_deletion'], df['json_deleted_words'] = df.apply(lambda df : pd.Series(test_fn(df['json_utterances_man'])), axis =1)
+df['json_man_with_deletion'] = df.apply(lambda df : del_fn(df['json_utterances_man']), axis =1)
+df.to_csv('C:/temp/DELETION_ASRforAD.csv')
 
-#read in csv file and store manual transcript in temp value
-df = pd.read_csv('C:/Users/toshiba/Documents/test.csv')
-print(df.head(3))
-tr = json.loads(df.iloc[0,2])
+print(df.head())
+print(len(df.iloc[2,5]))
+print(df.iloc[2,5])
 
-#function to flatten the transcript list
+
+# main fn 
+def del_fn(df):
+    return del_words(flatten(store_tr(df)), 0.2)
+
+#store json transcript
+def store_tr(df_row):       
+    tr = json.loads(df_row)
+    return tr
+
+#flatten the transcript list
 def flatten(transcript):    
     flat_list = []
     for sublist in transcript: 
         for i in sublist['tokens']:
-            flat_list.append(i)
+            flat_list.append(i)            
     return flat_list
-
-flat_tr = flatten(tr)
-print(len(flat_tr))
 
 # function to count number of words in transcript
 def word_count(transcript):
@@ -27,33 +38,20 @@ def word_count(transcript):
             cnt +=1    
     return cnt
 
-num_words = word_count(flat_tr)
-print(num_words)
-
-#create two lists: 1.deleted words, 2. indeces of deleted words
-del_rate = 0.2
-del_words = int(num_words * del_rate)
-ind_list=[]
-del_w = []
-
-while len(ind_list) < del_words:
-    r=random.randint(0,num_words)
-    if r not in list:                
-        if flat_tr[r]['type'] == 'word':
-            ind_list.append(r)
-            del_w.append([flat_tr[r]])
-    
-print(len(ind_list))
-print(len(del_w))
-print(del_w)
-print(ind_list)
-
-#create new transcript without the deleted words
-def new_tr(transcript, del_indx):    
+#delete random words from the transcript, return new transcript
+def del_words(transcript, del_rate): 
+    total_words = word_count(transcript)
+    del_words = int(total_words * del_rate)
+    del_indx=[]
+    del_w = []
+    while len(del_indx) < del_words:
+        r=random.randint(0,total_words)
+        if r not in del_indx:                
+            if transcript[r]['type'] == 'word':
+                del_indx.append(r)
+                del_w.append([transcript[r]])
     del_indx.sort()
-    new_tr = []
-    m = 0
-    i = 0
+    new_tr = []    
     for i in range (len(transcript)):
         if i not in del_indx:          
             new_tr.append(transcript[i])
