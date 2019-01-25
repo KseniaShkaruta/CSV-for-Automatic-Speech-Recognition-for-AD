615f21184b329f87d3b1efdfef70d787fbcd7755
Ksenia Shkaruta <46504542+KseniaShkaruta@users.noreply.github.com>
2019-01-24 23:00:47 -0500

    Change the format of the output list (tokens, start_time)
    Changes:
    1. Updated flatten(), word_count() and del_words() functions to keen 'tokens', 'start_time' and corresponding start time in the output transcript
    Outstanding:
    1. Label two new colums
    2. New transcript is in list format that contains 'tokens','words','start_time', start time value as its elements. Let me know if needs to be formatted  back to json
    3. New transcript still includes 'tokens' and 'start_time' for  words that were dropped and originally were one-token-word. I will fix that once know if json format is needed

--- DELETION.py
+++ DELETION.py
@@ -8,9 +8,6 @@ df = df.merge(df.json_utterances_man.apply(lambda s: pd.Series(del_fn(s))), left
 df.to_csv('C:/temp/DELETION_ASRforAD.csv')
 
 print(df.head())
-print(len(df.iloc[2,5]))
-print(df.iloc[2,5])
-
 
 # main fn 
 def del_fn(df):
@@ -22,22 +19,29 @@ def store_tr(df_row):
     return tr
 
 #flatten the transcript list
-def flatten(transcript):    
+def flatten(transcript):
     flat_list = []
-    for sublist in transcript: 
-        for i in sublist['tokens']:
-            flat_list.append(i)            
+    for sublist in transcript:
+        for item in sublist:
+            if item == 'tokens':
+                flat_list.append(item)
+                for item in sublist['tokens']:
+                    flat_list.append(item)   
+            else:
+                flat_list.append(item)
+                flat_list.append(str(sublist['start_time']))
     return flat_list
 
-# function to count number of words in transcript
+#count number of words in transcript
 def word_count(transcript):
     cnt = 0
-    for i in range(len(transcript)):
-        if transcript[i]['type'] == 'word':
-            cnt +=1    
+    for i in range(len(transcript)):        
+        if type(transcript[i]) is dict:
+            if transcript[i]['type'] == 'word':
+                cnt +=1    
     return cnt
 
-#delete random words from the transcript, return new transcript
+#delete random words from the transcript, return new transcript and the list of deleted words
 def del_words(transcript, del_rate): 
     total_words = word_count(transcript)
     del_words = int(total_words * del_rate)
@@ -45,11 +49,12 @@ def del_words(transcript, del_rate):
     del_w = []
     while len(del_indx) < del_words:
         r=random.randint(0,total_words)
-        if r not in del_indx:                
-            if transcript[r]['type'] == 'word':
-                del_indx.append(r)
-                del_w.append([transcript[r]])
-    del_indx.sort()
+        if r not in del_indx:
+            if type(transcript[r]) is dict:
+                if transcript[r]['type'] == 'word':
+                    del_indx.append(r)
+                    del_w.append([transcript[r]])
+        del_indx.sort()
     new_tr = []    
     for i in range (len(transcript)):
         if i not in del_indx:          
