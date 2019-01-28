29139b56d20e2b95a32b273fdff5aaeac50acfd6
Ksenia Shkaruta <46504542+KseniaShkaruta@users.noreply.github.com>
2019-01-27 23:13:34 -0500

    Output is now in JSON format
    Changes:
    1. Updated code for main and list_del_words fns and added del_words fn to do the word deletion from the unflattened transcript
    2. Named the new columns
    
    Considerations:
    1. I use while loop to iterate through the transcript and just handle the "out of index error" via exception with a random variable assignment. Not sure if this is robust and elegant code but I couldn't figure out how else to handle
    2. Deleting by value instead of index does deletion of the first occurring value which might be not at the index generated by random. It is still random deletion but I wonder if it defeats/diminishes the purpose of the random list generation

--- DELETION.py
+++ DELETION.py
@@ -2,16 +2,17 @@ import pandas as pd
 import json
 import random
 
-#read in csv file, delete 20% of words from json manual transcript, output csv with altered manual transcript as new column
+#read in csv file, delete 20% of words from json manual transcript, output csv with altered manual transcript and list of deleted words as new columns
 df = pd.read_csv('C:/temp/ASRforAD.csv')
 df = df.merge(df.json_utterances_man.apply(lambda s: pd.Series(del_fn(s))), left_index=True, right_index=True)
+df.rename(columns = {0:'json_utterances_man_wt_DELETED_WORDS', 1:'DELETED_WORDS'}, inplace =True )
 df.to_csv('C:/temp/DELETION_ASRforAD.csv')
 
 print(df.head())
 
 # main fn 
 def del_fn(df):
-    return del_words(flatten(store_tr(df)), 0.2)
+    return del_words(store_tr(df))
 
 #store json transcript
 def store_tr(df_row):       
@@ -33,30 +34,43 @@ def flatten(transcript):
     return flat_list
 
 #count number of words in transcript
-def word_count(transcript):
+def word_count(flat_transcript):
     cnt = 0
-    for i in range(len(transcript)):        
-        if type(transcript[i]) is dict:
-            if transcript[i]['type'] == 'word':
+    for i in range(len(flat_transcript)):        
+        if type(flat_transcript[i]) is dict:
+            if flat_transcript[i]['type'] == 'word':
                 cnt +=1    
     return cnt
 
-#delete random words from the transcript, return new transcript and the list of deleted words
-def del_words(transcript, del_rate): 
-    total_words = word_count(transcript)
+#create a list of words to delete from transcript using random seed
+def list_del_words(flat_transcript, del_rate): 
+    total_words = word_count(flat_transcript)
     del_words = int(total_words * del_rate)
     del_indx=[]
     del_w = []
     while len(del_indx) < del_words:
         r=random.randint(0,total_words)
         if r not in del_indx:
-            if type(transcript[r]) is dict:
-                if transcript[r]['type'] == 'word':
+            if type(flat_transcript[r]) is dict:
+                if flat_transcript[r]['type'] == 'word':
                     del_indx.append(r)
-                    del_w.append([transcript[r]])
-        del_indx.sort()
-    new_tr = []    
-    for i in range (len(transcript)):
-        if i not in del_indx:          
-            new_tr.append(transcript[i])
-    return del_w, new_tr
+                    del_w.append(flat_transcript[r])    
+    return del_w
+
+#delete words from the transcript, return the new transcript and list of deleted words
+def del_words(transcript):
+    to_delete = list_del_words(flatten(transcript), 0.2)
+    deleted_words = []        
+    i = 0
+    try:
+        while i != (len(to_delete)):
+            for sublist in transcript: 
+                for element in sublist['tokens']:
+                    if to_delete[i] == element:                  
+                        deleted_words.append(element)
+                        to_delete.remove(to_delete[i])
+                        sublist['tokens'].remove(element)                       
+            i =0
+    except:
+        a = 0
+    return transcript, deleted_words
