25a6df68383a1ef05fc69e7b93e9fe225d29e4eb
Ksenia Shkaruta <46504542+KseniaShkaruta@users.noreply.github.com>
2019-02-12 23:33:44 -0500

    Bi-gram dict
    Created bi-gram dictionary. It's only 39,138 key words long. The length of value lists varies and for some words can be up to several thousands (e.g. 'that' - 6,724 values)

--- INSERTION.py
+++ INSERTION.py
@@ -1,87 +1,33 @@
 from itertools import islice
 from heapq import nlargest
 
+#main function
+temp = create_bi_gram_dict(bi_gram_list)
+print(len(temp))
+
 #store bi-gram as a list of lists of three elements: word, following word and probability of occurance of the following word
-bi_gram=[]
-with open('../lm_unpruned', 'r') as f:
-    for line in islice(f, 57292, 1380576, 1): #read in words only from 1-gram list
-        bi_gram.append([float(line.split(' ')[0]), line.split(' ')[1], line.split(' ')[2].replace("\n", '')])
-        
-"""Quick stats
-2,000 - 100% unique, 1,598 after cleaning
-3,000 - 99.8% unique, 3 repeating word, 2,190 after cleaning
-4,000 - 99.5% unique, 10 repeating word, 2,914 after cleaning
-5,000 - 99.12% unique, 22 repeating word, 3,363 after cleaning
-6,000 - 99.3% unique, 22 repeating word, 3,363 after cleaning
-7,000 - 99.4% unique, 22 repeating word, 4,113 after cleaning
-8,000 - 99.4% unique, 22 repeating word, 4,987 after cleaning
+def read_bi_gram():
+    bi_gram=[]
+    
+    with open('../lm_unpruned', 'r') as f:
+        for line in islice(f, 57292, 1380576, 1): #read in words only from 1-gram list
+            if line.split(' ')[1] not in ('<unk>', '</s>') and '_' not in line.split(' ')[1] and line.split(' ')[2].replace("\n", '') not in ('<unk>', '</s>') and '_' not in line.split(' ')[2].replace("\n", ''):
+                bi_gram.append([float(line.split(' ')[0]), line.split(' ')[1], line.split(' ')[2].replace("\n", '')])
+    
+    return bi_gram
+
+bi_gram_list = read_bi_gram()
+print(len(bi_gram))
 
 
-#For 2,000 - 100% unique, 1,598 after cleaning
-words = nlargest(2000, bi_gram)
-unique = []
-repeat = []
-for i in range(len(words)):
-    if words[i][1] not in unique:
-        unique.append(words[i][1])
-    else:
-        print(words[i])
+#create bi-gram dictionary based on the list of bi-grams
+def create_bi_gram_dict(bi_gram_list):
+    bi_gram_dict = {}
 
-#code to clean the bi-gram list 
-reduced = []
-for i in range(len(words)):
-    if "_" not in words[i][1]:
-        if words[i][2] != '<unk>':
-            if words[i][2] != '</s>':
-                reduced.append(words[i])
-print(len(unique))
-print(repeat)
-print(len(reduced))
-
-
-#For 3,000 - 2,994 unique, 3 - repeating
-words = nlargest(3000, bi_gram)
-unique = []
-repeat = []
-for i in range(len(words)):
-    if words[i][1] not in unique:
-        unique.append(words[i][1])
-    else:
-        print(words[i])
-        
-#code to clean the bi-gram list 
-reduced = []
-for i in range(len(words)):
-    if "_" not in words[i][1]:
-        if words[i][2] != '<unk>':
-            if words[i][2] != '</s>':
-                reduced.append(words[i])
-print(len(unique))
-print(repeat)
-print(len(reduced))
-
-#For 8,000 - 7,948 unique, 26 - repeating
-words = nlargest(8000, bi_gram)
-unique = []
-repeat = 0
-for i in range(len(words)):
-    if words[i][1] not in unique:
-        unique.append(words[i][1])
-    else:
-        print(words[i])
-        repeat +=1
-        
-#code to clean the bi-gram list 
-reduced = []
-for i in range(len(words)):
-    if "_" not in words[i][1]:
-        if words[i][2] != '<unk>':
-            if words[i][2] != '</s>':
-                reduced.append(words[i])
-                
-print(len(unique))
-print(repeat)
-print(len(reduced))
-
-for i in range(len(reduced)):
-    print(reduced[i])
+    for i in range(len(bi_gram)): 
+        if bi_gram[i][1] in bi_gram_dict.keys():  
+            bi_gram_dict[bi_gram[i][1]].append([bi_gram[i][0], bi_gram[i][2]])  
+        else:  
+            bi_gram_dict[bi_gram[i][1]] = [[bi_gram[i][0], bi_gram[i][2]]]
+    
+    return bi_gram_dict 
