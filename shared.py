8b001423ea9c6df94e72e0e303a17381e648075d
Ksenia Shkaruta <46504542+KseniaShkaruta@users.noreply.github.com>
2019-02-16 20:32:21 -0500

    JuputerLab + shared.py
    1. Pulled shared functions into shared.py
    2. Uploaded final code as .ipnyb from Jupyter Lab

---
+++ shared.py
@@ -0,0 +1,60 @@
+import json
+import random
+from itertools import islice
+from heapq import nlargest
+
+#store json manual transcript from dataframe
+def store_tr(df_row):       
+    tr = json.loads(df_row)
+    return tr
+
+
+#flatten the transcript into a list
+def flatten(transcript):
+    flat_list = []
+    for sublist in transcript:
+        for item in sublist:
+            if item == 'tokens':
+                flat_list.append(item)
+                for item in sublist['tokens']:
+                    flat_list.append(item)   
+            else:
+                flat_list.append(item)
+                flat_list.append(str(sublist['start_time']))
+    return flat_list
+
+
+#count number of words in flat transcript
+def word_count(flat_transcript):
+    cnt = 0
+    word_indx = [] # initialise list of all the word indices
+    for i in range(len(flat_transcript)):        
+        if type(flat_transcript[i]) is dict:
+            if flat_transcript[i]['type'] == 'word':
+                cnt +=1    
+                word_indx.append(i) # add word indices here and you won't need to go through the transcript once again to collect them
+    return cnt, word_indx
+
+
+#create a list of random words from transcript for deletion/substitution/insertion
+def random_words_list(flat_transcript, rate): 
+    total_words, indx = word_count(flat_transcript)
+    words = int(total_words * rate)
+    indx = random.sample(population=indx, k=words) # select random indices for deletion from the list of all word indices
+    words_list = []
+    for i in indx:
+        words_list.append(flat_transcript[i]['value'])
+    return words_list
+
+
+#create a 1-gram words list
+def create_one_gram_list():
+    one_gram=[]
+    with open('../lm_unpruned', 'r') as f:
+        for line in islice(f, 8, 42159, 1): #read in words only from 1-gram list
+            if line.split(' ')[1] not in ('<unk>', '</s>'):
+                one_gram.append([float(line.split(' ')[0]), line.split(' ')[1].replace("\n", '')])   
+    return one_gram
+
+one_gram_list = create_one_gram_list()
+reduced_one_gram = nlargest(2000, one_gram_list) 
